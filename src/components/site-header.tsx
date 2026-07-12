419b72fa9b4566968892ca1036bddf0dd43c3ba1
William Elesbao <william.elesbao.2000@gmail.com>
2026-07-12 01:33:58 -0300

    refactor: simplify SiteHeader component by removing subscription props and updating title

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,21 +1,12 @@
-import type { Subscription } from "@better-auth/stripe";
 import { IconBrandGithub } from "@tabler/icons-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { CancelSubscriptionDialog } from "./cancel-plan-dialog";
+import { SidebarTrigger } from "@/features/sidebar/components/sidebar";
 import { LanguageSwitcher } from "./language-switcher";
 import { ThemeToggle } from "./theme-toggle";
 
-interface SiteHeaderProps {
-  subscription?: Subscription;
-}
-
-export function SiteHeader({ subscription }: Readonly<SiteHeaderProps>) {
-  const shouldShowRevokeButton =
-    subscription?.status === "active" && !subscription?.cancelAt;
-
+export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -24,7 +15,7 @@ export function SiteHeader({ subscription }: Readonly<SiteHeaderProps>) {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="font-medium text-base">Dashboard</h1>
+        <h1 className="font-medium text-base">Subscription</h1>
         <div className="ml-auto flex items-center gap-2">
           <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <Link
@@ -35,8 +26,6 @@ export function SiteHeader({ subscription }: Readonly<SiteHeaderProps>) {
               <IconBrandGithub className="size-5" />
             </Link>
           </Button>
-
-          {shouldShowRevokeButton && <CancelSubscriptionDialog />}
 
           <LanguageSwitcher />
           <ThemeToggle />
